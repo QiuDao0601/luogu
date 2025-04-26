167e03ebef71319d8bc6013ca35268f2c8bb8119
朗 于 <2317095145@qq.com>
2025-04-26 23:01:35 +0800

    添加了P1002

---
+++ P1002/P1002.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	double sum=0;
+	int n=1,k;
+	cin >> k;
+	while (sum <= k)
+	{
+		sum += 1.0 / n;
+		n++;
+	}
+	cout << n-1;
+
+	return 0;
+}
