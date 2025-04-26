8268b14eb2f695b648d5b449f8ace4988ae3fc4c
朗 于 <2317095145@qq.com>
2025-04-26 23:20:11 +0800

    重新调整仓库结构

--- 入门/P1035/P1035.cpp
+++ 入门/P1035/P1035.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 int main()
 {
-	double sum=0;
-	int n=1,k;
+	double sum = 0;
+	int n = 1, k;
 	cin >> k;
 	while (sum <= k)
 	{
 		sum += 1.0 / n;
 		n++;
 	}
-	cout << n-1;
+	cout << n - 1;
 
 	return 0;
 }
