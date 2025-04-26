5df6d3474cdd13ef28859732040b580944540e14
朗 于 <2317095145@qq.com>
2025-04-26 23:27:39 +0800

    添加了P1046

---
+++ 入门/P1046/P1046.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a[10], h, count = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		cin >> a[i];
+	}
+	cin >> h;
+	for (int i = 0; i < 10; i++)
+	{
+		if (a[i] <= h + 30)
+		{
+			count++;
+		}
+	}
+	cout << count;
+
+	return 0;
+}
