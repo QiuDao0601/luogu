980091307e734ff1769c8a714abe74c0419f785a
朗 于 <2317095145@qq.com>
2025-04-27 09:43:10 +0800

    修改了P1047

--- 入门/P1047/P1047.cpp
+++ 入门/P1047/P1047.cpp
@@ -3,7 +3,34 @@ using namespace std;
 
 int main()
 {
+	int n, arr[10000],a,arr1[100],arr2[100],count=0;
+	cin >> n >> a;
+	for (int i = 0; i < a; i++)
+	{
+		cin >> arr1[i] >> arr2[i];
+	}
 
+	for (int i = 0; i <= n; i++)
+	{
+		arr[i] = 1;
+	}
+
+	for (int i = 0; i < a; i++)
+	{
+		for (int j = arr1[i]; j <= arr2[i]; j++)
+		{
+			arr[j] = 0;
+		}
+	}
+
+	for (int i = 0; i <= n; i++)
+	{
+		if (arr[i] == 1)
+		{
+			count++;
+		}
+	}
+	cout << count;
 
 	return 0;
 }
