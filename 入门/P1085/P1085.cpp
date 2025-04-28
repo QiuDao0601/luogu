319824cd7b948dc89f76145df8c8a8dc5779c1ef
朗 于 <2317095145@qq.com>
2025-04-28 21:14:54 +0800

    添加了P1085,P1089

---
+++ 入门/P1085/P1085.cpp
@@ -0,0 +1,25 @@
+﻿//P1085 [NOIP 2004 普及组] 不高兴的津津
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int arr[2][7], sum=0, i, day = -1;
+	for (int i=0;i<7;i++)
+	{
+		cin >> arr[0][i] >> arr[1][i];
+	}
+
+	for (i = 0; i < 7; i++)
+	{
+		if (arr[0][i] + arr[1][i] > 8 && arr[0][i] + arr[1][i] > sum)
+		{
+			day = i;
+			sum = arr[0][i] + arr[1][i];
+		}
+	}
+
+	cout << day + 1;
+
+	return 0;
+}
---
+++ 入门/P1089/P1089.cpp
@@ -0,0 +1,40 @@
+﻿//P1089 [NOIP 2004 提高组] 津津的储蓄计划
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	double  current = 0, save = 0;
+	int left = 0,i,arr[12];
+	bool check = false;
+
+	for (int i = 0; i < 12; i++)
+	{
+		cin >> arr[i];
+	}
+
+	for (i = 0; i < 12; i++)
+	{
+		current = left + 300;
+		left = current - arr[i];
+		if (left < 0)
+		{
+			check = true;
+			break;
+		}
+		save += 100 * (left / 100);
+		left = left % 100;
+		current = left;
+	}
+
+	if (check)
+	{
+		cout <<  (-1) * i - 1;
+	}
+	else
+	{
+		cout << save * 1.2 + left;
+	}
+
+	return 0;
+}
